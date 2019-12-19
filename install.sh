255cd6be8ffc8061351a34a9bf7e51f242f6e16e
dunfrey@gmail.com <dunfrey@gmail.com>
2019-12-19 15:01:15 -0300

    redocumenting

--- install.sh
+++ install.sh
@@ -13,15 +13,20 @@ cd anacondainstaller
 wget https://repo.anaconda.com/archive/Anaconda3-5.1.0-Linux-x86_64.sh
 chmod +x Anaconda3-5.1.0-Linux-x86_64.sh
 ./Anaconda3-5.1.0-Linux-x86_64.sh
+echo ". /home/ubuntu/anaconda3/etc/profile.d/conda.sh" >> ~/.bashrc
+conda activate
 python --version
 
 # installing spark
+cd ~
 wget https://www-eu.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
-tar -xvf spark-2.4.4-bin-hadoop2.7.tgz -C /spark
+mkdir ~/spark
+tar -xvf spark-2.4.4-bin-hadoop2.7.tgz -C ~/spark
 export SPARK_HOME=~/spark/spark-2.4.4-bin-hadoop2.7/
 export PATH="$SPARK_HOME/bin:$PATH"
 source ~/.bashrc
 
 # installing pip
+conda activate
 sudo apt install python3-pip
 pip install -r requirements.txt
